758a5e989a2d12df24a31e78968c6d2d5e317ba4
HawkeHH <145706583+HawkeHH@users.noreply.github.com>
2023-10-07 13:35:26 -0400

    Update MainActivity.java
    App bar included: still needs call for a new game/ yes no dialogue option.
    
    Generic placehoders/ listeners added. feel free to change/ remove as needed or for preference.

--- TikTacToe/app/src/main/java/com/zybooks/tiktactoe/MainActivity.java
+++ TikTacToe/app/src/main/java/com/zybooks/tiktactoe/MainActivity.java
@@ -3,13 +3,62 @@ package com.zybooks.tiktactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TicTakToeGame game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // hello 
+        game = new TicTakToeGame();
+
+        // Initialize the game grid
+        GridLayout gridLayout = findViewById(R.id.tiktok_grid);
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            Button button = (Button) gridLayout.getChildAt(i);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onGridButtonClick((Button) v);
+                }
+            });
+        }
+    }
+
+    private void onGridButtonClick(Button button) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.action_Newgame) {
+
+            Toast.makeText(this, "Restarting the game", Toast.LENGTH_SHORT).show();
+             // need to add newgame() call once made.
+
+
+            return true;
+        } else if (itemId == R.id.action_Exit) {
+            // Exit the app
+            // need to add dioloauge accepte decln message.
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
